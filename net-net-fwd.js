c9be6fb3da04ee9f457a86beb26c7a25e18e9ffb
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-09 10:00:20 +0900

    space to tab

--- net-net-fwd.js
+++ net-net-fwd.js
@@ -7,54 +7,54 @@ var PROXY_PORT = process.argv[4] || 13389;    // proxy server port
 if (!PROXY_HOST) return console.log('proxy server not found');
 
 var server = net.createServer(function onCliConn(cliSoc) {
-  var svrMsgCnt = 0, cliMsgCnt = 0;
-  console.log('%s cliSoc: connected!', new Date().toLocaleTimeString());
-  var svrSoc = net.connect(PROXY_PORT, PROXY_HOST, function onSvrConn(err) {
-    if (err)
-      console.log('%s svrCon: %s', new Date().toLocaleTimeString(), err);
+	var svrMsgCnt = 0, cliMsgCnt = 0;
+	console.log('%s cliSoc: connected!', new Date().toLocaleTimeString());
+	var svrSoc = net.connect(PROXY_PORT, PROXY_HOST, function onSvrConn(err) {
+		if (err)
+			console.log('%s svrCon: %s', new Date().toLocaleTimeString(), err);
 
-    //svrSoc.pipe(cliSoc);
-    svrSoc.on('readable', function () {
-      var buff = svrSoc.read();
-      if (!buff) return;
-      ++svrMsgCnt;
-      //if (svrMsgCnt <= 5)
-      //  console.log('svr ' + svrMsgCnt + ': ' + buff.length);
-      cliSoc.write(buff);
-    });
-    svrSoc.on('end', function () { cliSoc.end();});
+		//svrSoc.pipe(cliSoc);
+		svrSoc.on('readable', function () {
+			var buff = svrSoc.read();
+			if (!buff) return;
+			++svrMsgCnt;
+			//if (svrMsgCnt <= 5)
+			//  console.log('svr ' + svrMsgCnt + ': ' + buff.length);
+			cliSoc.write(buff);
+		});
+		svrSoc.on('end', function () { cliSoc.end();});
 
-    //cliSoc.pipe(svrSoc);
-    cliSoc.on('readable', function () {
-      var buff = cliSoc.read();
-      if (!buff) return;
-      ++cliMsgCnt;
-      if (cliMsgCnt <= 3)
-        console.log('cli ' + cliMsgCnt + ': ' + buff.length);
-      if (cliMsgCnt <= 1)
-        console.log('cli ' + cliMsgCnt + ':', buff);
-      svrSoc.write(buff);
-    });
-    cliSoc.on('end', function () { svrSoc.end(); });
-  });
-  cliSoc.on('error', function (err) {
-    console.log('%s cliSoc: %s', new Date().toLocaleTimeString(), err);
-    svrSoc.destroy();
-  });
-  svrSoc.on('error', function (err) {
-    console.log('%s svrSoc: %s', new Date().toLocaleTimeString(), err);
-    cliSoc.destroy();
-  });
+		//cliSoc.pipe(svrSoc);
+		cliSoc.on('readable', function () {
+			var buff = cliSoc.read();
+			if (!buff) return;
+			++cliMsgCnt;
+			if (cliMsgCnt <= 3)
+				console.log('cli ' + cliMsgCnt + ': ' + buff.length);
+			if (cliMsgCnt <= 1)
+				console.log('cli ' + cliMsgCnt + ':', buff);
+			svrSoc.write(buff);
+		});
+		cliSoc.on('end', function () { svrSoc.end(); });
+	});
+	cliSoc.on('error', function (err) {
+		console.log('%s cliSoc: %s', new Date().toLocaleTimeString(), err);
+		svrSoc.destroy();
+	});
+	svrSoc.on('error', function (err) {
+		console.log('%s svrSoc: %s', new Date().toLocaleTimeString(), err);
+		cliSoc.destroy();
+	});
 }).listen(HTTP_PORT, function () {
-  process.on('uncaughtException', function (err) {
-    var msg1 = /\n    at exports._errnoException \(util.js:\d*:\d*\)\n    at TCP.onread \(net.js:\d*:\d*\)/;
-    console.log('uncExc %s', err.stack.replace(msg1, ''));
-  });
+	process.on('uncaughtException', function (err) {
+		var msg1 = /\n    at exports._errnoException \(util.js:\d*:\d*\)\n    at TCP.onread \(net.js:\d*:\d*\)/;
+		console.log('uncExc %s', err.stack.replace(msg1, ''));
+	});
 });
 
 server.on('error', function onSvrErr(err) {
-  console.log('%s svrErr: %s', new Date().toLocaleTimeString(), err);
+	console.log('%s svrErr: %s', new Date().toLocaleTimeString(), err);
 }); // server.on error
 
 console.log('port forwarder started on port ' + HTTP_PORT +
-            ' -> ' + PROXY_HOST + ':' + PROXY_PORT);
+						' -> ' + PROXY_HOST + ':' + PROXY_PORT);
--- web8080.js
+++ web8080.js
@@ -4,19 +4,19 @@ var port = 8080;
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log([req.method, req.url].join(' '));
-  res.end('Hello World! port:' + port + '\n');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	console.log([req.method, req.url].join(' '));
+	res.end('Hello World! port:' + port + '\n');
 });
 server.listen(port, function () {
-  console.log('Server running at http://localhost:' + port);
+	console.log('Server running at http://localhost:' + port);
 
-  server.on('connection', function (soc) {
-    console.log('server connected ');
-    soc.on('disconnect', function () {
-      console.log('server disconnected ');
-    });
-  });
+	server.on('connection', function (soc) {
+		console.log('server connected ');
+		soc.on('disconnect', function () {
+			console.log('server disconnected ');
+		});
+	});
 });
 
 console.log('Server starting at http://localhost:' + port);

14ea40272fd548075644a0f28d085870d1d863ef
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-03-10 20:49:32 +0900

    add hostname

--- web8080.js
+++ web8080.js
@@ -2,11 +2,13 @@
 
 var port = 8080;
 var http = require('http');
+var host = process.env.COMPUTERNAME.toLowerCase() ||
+	process.env.HOSTNAME.toLowerCase();
 
 var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	console.log([req.method, req.url].join(' '));
-	res.end('Hello World! port:' + port + '\n');
+	res.end('Hello World! host:' + host + ', port:' + port + '\n');
 });
 server.listen(port, function () {
 	console.log('Server running at http://localhost:' + port);
